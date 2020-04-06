cb8df0f095c5a59baf5ce5bcd4e4dfc8f6ce561f
ihsanyassin2 <61631984+ihsanyassin2@users.noreply.github.com>
2020-04-07 00:02:59 +0800

    Update pix2pix.py

--- pix2pix.py
+++ pix2pix.py
@@ -1,3 +1,23 @@
+## clone this repo
+#git clone https://github.com/affinelayer/pix2pix-tensorflow.git
+#cd pix2pix-tensorflow
+## download the CMP Facades dataset (generated from http://cmp.felk.cvut.cz/~tylecr1/facade/)
+#python tools/download-dataset.py facades
+## train the model (this may take 1-8 hours depending on GPU, on CPU you will be waiting for a bit)
+#python pix2pix.py \
+#  --mode train \
+#  --output_dir facades_train \
+#  --max_epochs 200 \
+#  --input_dir facades/train \
+#  --which_direction BtoA
+## test the model
+#python pix2pix.py \
+#  --mode test \
+#  --output_dir facades_test \
+#  --input_dir facades/val \
+#  --checkpoint facades_train
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
